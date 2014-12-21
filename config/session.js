5815453111e7f32e182189881a38fee5017e976b
laurenceputra <laurenceputra@gmail.com>
2014-12-21 08:34:08 +0800

    added persistent sessions

--- config/session.js
+++ config/session.js
@@ -9,8 +9,12 @@ module.exports = function(app){
             url : mongodb['uri'],
             auto_reconnect: true,
             resave: true,
-            saveUninitialized: true
-        })
+            saveUninitialized: false
+        }),
+        cookie: {
+            expires: (new Date(Date.now() + (60000 * 24 * 30))),
+            maxAge: (60000 * 24 * 30)
+        }
       })
     );
 }
