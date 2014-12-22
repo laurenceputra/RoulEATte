764d9d50e7ad9e4cf08194452c33f7a7b841ff4c
laurenceputra <laurenceputra@gmail.com>
2014-12-22 08:25:55 +0800

    fixed session arguments

--- config/session.js
+++ config/session.js
@@ -8,13 +8,13 @@ module.exports = function(app){
         store: new MongoStore({
             url : mongodb['uri'],
             auto_reconnect: true,
-            resave: true,
-            saveUninitialized: false
         }),
         cookie: {
             expires: (new Date(Date.now() + (60000 * 24 * 30))),
             maxAge: (60000 * 24 * 30)
-        }
+        },
+        resave: true,
+        saveUninitialized: true
       })
     );
 }
